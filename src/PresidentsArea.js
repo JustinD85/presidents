bfaf1b32f02012189f3e1a0cd760a172e2e7f4c4
Justin <justin@baal-software.com>
2019-02-26 12:01:06 -0700

    Add filtered list, non functional

--- src/PresidentsArea.js
+++ src/PresidentsArea.js
@@ -6,19 +6,39 @@ const styles = {
     flexWrap: 'wrap',
   },
   card: {
-    margin: 120,
+    margin: 8,
+    minWidth: 250,
     background: 'lightgrey',
     padding: 10,
     borderRadius: 10,
+  },
+  filter: {
+    height: 100,
+    margin: '0 auto',
   },
   loading: { margin: '0 auto' },
 }
 export const PresidentsArea = ({ presidents, hasErrored }) => {
   if (hasErrored) {
-    return <h1 className="error" style={{ margin: '0 auto' }}>There is an error...</h1>
+    return (
+      <h1 className="error" style={{ margin: '0 auto' }}>
+        There is an error...
+      </h1>
+    )
   } else if (presidents) {
+    const ufilter = presidents.map(p => p.party)
+    const list = ufilter.filter(
+      (word, index) => ufilter.findIndex(iWord => iWord === word) === index,
+    )
     return (
       <div className="presidents" style={styles.root}>
+        <div style={styles.filter}>
+          <select>
+            {list.map(party => (
+              <option>{party}</option>
+            ))}
+          </select>
+        </div>
         {presidents.map(president => (
           <Fragment key={president.toString + 'key'}>
             <div style={styles.card} key={president.toString()}>
